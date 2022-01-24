461d1e9aa78960e1227432925de77c31858763fb
burinson <garentei@gmail.com>
2022-01-23 20:29:27 -0600

    Added neural network

--- main.py
+++ main.py
@@ -9,20 +9,63 @@ import tensorflow
 import random
 import json
 
+nltk.download('punkt')
+
 with open("intents.json") as file:
     data = json.load(file)
 
-print(data)
-
-words = []
-labels = []
-docs = []
+words = []  # all words
+labels = [] # unique tags?
+docs_x = [] # patterns
+docs_y = [] # tag (category)
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
-        words.extend(nltk.word_tokenize(pattern))
-        docs.append(pattern)
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
 
         if intent["tag"] not in labels:
             labels.append(intent["tag"])
 
+words = [stemmer.stem(w.lower()) for w in words if w != "?"] # stem all words
+words = sorted(list(set(words))) # make unique and sort
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+# 1-hot encoding
+for x, doc in enumerate(docs_x):
+    bag = []
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+output = numpy.array(output)
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8) # 8 neurons for hidden layer
+net = tflearn.fully_connected(net, 8) # 8 neurons for hidden layer
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
